87d13f5d8feddf7c2aa08fb25893ab239349ab93
halino2710 <kemokaihamed2710@gmail.com>
2024-04-03 14:06:44 +0000

    Modify Task 2: Print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,31 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-
-    for i in range(x):
+    i = 0
+    for j in range(x):
         try:
-            print("{:d}".format(my_list[i]), end='')
-        except TypeError:
-            pass
-        except ValueError:
-            pass
-        else:
-            count += 1
-
+            print("{:d}".format(my_list[j]), end="")
+            i += 1
+        except(ValueError, TypeError):
+            continue
     print()
-    return (count)#!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end='')
-        except TypeError:
-            pass
-        except ValueError:
-            pass
-        else:
-            count += 1
-
-    print()
-    return (count)
+    return i
